5e4d918c36f0d95fade7ab85f2428977db0ac46b
Alies Lapatsin <web.lapateen@gmail.com>
2025-03-17 17:06:39 +0100

    Extend test fixtures

--- tests/Commands/sync_lang_files/es/a.php
+++ tests/Commands/sync_lang_files/es/a.php
@@ -6,4 +6,9 @@ return [
     'group' => [
         'Help' => 'Ayuda',
     ],
+
+    // test a numeric key, @see https://github.com/diglabby/laravel-find-missing-translations/pull/10
+    '404' => [
+        'title' => '404 :: Página no encontrada',
+    ],
 ];
--- tests/Commands/sync_lang_files/fr/a.php
+++ tests/Commands/sync_lang_files/fr/a.php
@@ -6,4 +6,9 @@ return [
     'group' => [
         'Help' => 'Aide',
     ],
+
+    // test a numeric key, @see https://github.com/diglabby/laravel-find-missing-translations/pull/10
+    '404' => [
+        'title' => '404 :: Page non trouvée',
+    ],
 ];
